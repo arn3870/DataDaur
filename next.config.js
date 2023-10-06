94bd6f76992118d56f2f979e5e3ff149135b9b09
Arslan Baig <arn3870@gmail.com>
2023-10-06 22:33:31 +0500

    updated config files to show images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["https://data-daur-web.vercel.app/"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
