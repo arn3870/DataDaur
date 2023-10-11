d042f6509cc09f07104f30f79b8727d49b6c8293
Arslan Baig <arn3870@gmail.com>
2023-10-11 17:14:51 +0500

    added contact form responsivness

--- src/components/contactForm/ContactForm.tsx
+++ src/components/contactForm/ContactForm.tsx
@@ -1,77 +1,54 @@
+import styles from "./contactForm.module.css";
 export default function ContactForm() {
   return (
     <div>
-      <div className="text-center text-[60px] lg:py-[80px] lg:px-[200px]">
-        Ready to do business with us? Let’s Get Started!
+      <div className="text-center text-[28px] md:text-[60px] lg:text-[60px] py-[20px] md:py-[50px] lg:py-[80px] px-[20px] md:px-[150px] lg:px-[200px]">
+        Ready to do business with us? Let s Get Started!
       </div>
-      <div className="w-full max-h-screen flex justify-center items-center h-full">
+      <div className="w-full flex justify-center items-center h-full">
         <div className="rounded-lg shadow xl:w-[875px] md:w-[700px] md:top-[-25px] w-full">
           <form>
-            <div className="pr-5">
+            <div className="px-[20px] md:pr-5 lg:pr-5">
               <div className="flex xl:flex-nowrap flex-wrap justify-between py-[10px] gap-5 xl:gap-16">
                 <div className="flex flex-col w-full">
-                  <label className="pb-[4px]">What is your first name?</label>
+                  <label className="pb-[4px] text-[12px] md:text-[16px] lg:text-[18px]">
+                    What is your first name?
+                  </label>
                   <input
-                    className="pl-[10px] lg:mt-[15px] focus:border-[#C5CBDC] focus:border-[2px] outline-none focus:outline-none py-[20px] px-[20px] border-[2px] rounded-[7px] text-[12px] sm:text-[14px]"
+                    className={` ${styles.inputClass} pl-[10px] lg:mt-[15px] outline-none focus:outline-none py-[10px] md:py-[20px] lg:py-[20px] px-[20px] rounded-[7px] text-[12px] sm:text-[14px]`}
                     id="service"
-                    style={{
-                      borderRadius: "20px",
-                      border: "1px solid var(--Button-Stroke, #A265F0)",
-                      background:
-                        "linear-gradient(143deg, rgba(255, 255, 255, 0.06) 0%, rgba(255, 255, 255, 0.02) 99.97%)",
-                      boxShadow: "0px 4px 30px 0px rgba(100, 26, 135, 0.50)",
-                      backdropFilter: "blur(5px)",
-                    }}
                   ></input>
                 </div>
                 <div className="flex flex-col w-full">
-                  <label className="pb-[4px]">What is your last name?</label>
+                  <label className="pb-[4px] text-[12px] md:text-[16px] lg:text-[18px]">
+                    What is your last name?
+                  </label>
                   <input
-                    className="pl-[10px] lg:mt-[15px] focus:border-[#C5CBDC] focus:border-[2px] outline-none focus:outline-none py-[20px] px-[20px] border-[2px] border-[#C5CBDC] rounded-[7px] text-[12px] sm:text-[14px]"
+                    className={` ${styles.inputClass} pl-[10px] lg:mt-[15px] outline-none focus:outline-none py-[10px] md:py-[20px] lg:py-[20px] px-[20px] rounded-[7px] text-[12px] sm:text-[14px]`}
                     id="review_actions"
-                    style={{
-                      borderRadius: "20px",
-                      border: "1px solid var(--Button-Stroke, #A265F0)",
-                      background:
-                        "linear-gradient(143deg, rgba(255, 255, 255, 0.06) 0%, rgba(255, 255, 255, 0.02) 99.97%)",
-                      boxShadow: "0px 4px 30px 0px rgba(100, 26, 135, 0.50)",
-                      backdropFilter: "blur(5px)",
-                    }}
                   ></input>
                 </div>
               </div>
               <div className="flex xl:flex-nowrap flex-wrap justify-between py-[10px] gap-5 xl:gap-16">
                 <div className="flex flex-col w-full">
-                  <label className="pb-[4px]">What is your email?</label>
+                  <label className="pb-[4px] text-[12px] md:text-[16px] lg:text-[18px]">
+                    What is your email?
+                  </label>
                   <input
-                    className="pl-[10px] lg:mt-[15px] focus:border-[#C5CBDC] focus:border-[2px] outline-none focus:outline-none py-[20px] px-[20px] border-[2px] border-[#C5CBDC] rounded-[7px] text-[12px] sm:text-[14px]"
+                    className={` ${styles.inputClass} pl-[10px] lg:mt-[15px] outline-none focus:outline-none py-[10px] md:py-[20px] lg:py-[20px] px-[20px] rounded-[7px] text-[12px] sm:text-[14px]`}
                     id="review_actions"
-                    style={{
-                      borderRadius: "20px",
-                      border: "1px solid var(--Button-Stroke, #A265F0)",
-                      background:
-                        "linear-gradient(143deg, rgba(255, 255, 255, 0.06) 0%, rgba(255, 255, 255, 0.02) 99.97%)",
-                      boxShadow: "0px 4px 30px 0px rgba(100, 26, 135, 0.50)",
-                      backdropFilter: "blur(5px)",
-                    }}
                   ></input>
                 </div>
               </div>
               <div className="flex xl:flex-nowrap flex-wrap justify-between py-[10px] gap-5 xl:gap-16">
                 <div className="flex flex-col w-full">
-                  <label className="pb-[4px]">Tell us about your enquiry</label>
+                  <label className="pb-[4px] text-[12px] md:text-[16px] lg:text-[18px]">
+                    Tell us about your enquiry
+                  </label>
                   <textarea
                     rows={10}
-                    className="pl-[10px] lg:mt-[15px] focus:border-[#C5CBDC] focus:border-[2px] outline-none focus:outline-none py-[20px] px-[20px] border-[2px] border-[#C5CBDC] rounded-[7px] text-[12px] sm:text-[14px]"
+                    className={` ${styles.inputClass} pl-[10px] lg:mt-[15px] outline-none focus:outline-none py-[10px] md:py-[20px] lg:py-[20px] px-[20px] rounded-[7px] text-[12px] sm:text-[14px]`}
                     id="review_actions"
-                    style={{
-                      borderRadius: "20px",
-                      border: "1px solid var(--Button-Stroke, #A265F0)",
-                      background:
-                        "linear-gradient(143deg, rgba(255, 255, 255, 0.06) 0%, rgba(255, 255, 255, 0.02) 99.97%)",
-                      boxShadow: "0px 4px 30px 0px rgba(100, 26, 135, 0.50)",
-                      backdropFilter: "blur(5px)",
-                    }}
                   ></textarea>
                 </div>
               </div>
@@ -84,13 +61,13 @@ export default function ContactForm() {
                     "var(--Button-Filled--Gradient, linear-gradient(89deg, #3E2567 0.92%, #140769 98.29%))",
                   boxShadow: "0px 4px 25px 0px #2F1C64",
                 }}
-                className="sm:px-[10px] md:px-[30px] lg:px-[50px] sm:py-[10px] md:py-[10px] lg:py-[10px] rounded-[20px] color-white"
+                className="px-[20px] md:px-[30px] lg:px-[50px] py-[10px] md:py-[10px] lg:py-[10px] rounded-[20px] color-white"
               >
                 Submit
               </button>
             </div>
             <div
-              className="w-[100%] h-[4px] lg:mt-[100px] lg:mt-[50px]"
+              className=" w-[100%] h-[4px] mt-[20px] md:mt-[50px] lg:mt-[100px]"
               style={{
                 background:
                   "linear-gradient(89deg, #3F1B79 0.92%, #11074F 98.29%)",
